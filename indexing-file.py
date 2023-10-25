92de72b07b113f53d58274e7eb2179d6ccbe6212
Chamod Madhusanka <chamodmadushanka115@gmail.com>
2023-10-26 01:14:21 +0530

    Update the indexing-dile

--- indexing-file.py
+++ indexing-file.py
@@ -16,7 +16,7 @@ poem_corpus_df.head()
 
 """## Connect to elasticsearch"""
 
-ENDPOINT = "https://9f52-203-189-185-168.ngrok.io:443"
+ENDPOINT = ""  # add running elasticsearch URL here
 USERNAME = ""
 PASSWORD = ""
 
@@ -28,32 +28,32 @@ es.ping()
 
 # PUT posting
 config = {
-   "settings":{
-      "number_of_shards":1,
-      "number_of_replicas":0
-   },
-   "mappings":{
-      "properties":{
-         "poem_name":{
-            "type":"text"
-         },
-         "poet":{
-            "type":"text",
-         },
-         "line":{
-            "type":"text"
-         },
-         "metaphor_present":{
-            "type":"text"
-         },
-         "metaphor_count":{
-            "type":"text"
-         },
-         "metaphorical_terms":{
-            "type":"text"
-         },
-      }
-   }
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    },
+    "mappings": {
+        "properties": {
+            "poem_name": {
+                "type": "text"
+            },
+            "poet": {
+                "type": "text",
+            },
+            "line": {
+                "type": "text"
+            },
+            "metaphor_present": {
+                "type": "text"
+            },
+            "metaphor_count": {
+                "type": "text"
+            },
+            "metaphorical_terms": {
+                "type": "text"
+            },
+        }
+    }
 }
 
 es.indices.create(
@@ -67,18 +67,18 @@ es.indices.create(
 # Fill all potential None or NaN values with empty strings
 poem_corpus_df.fillna("", inplace=True)
 
-for i,row in poem_corpus_df.iterrows():
+for i, row in poem_corpus_df.iterrows():
 
     try:
-      doc = {
-          "poem_name": row["Poem Name"],
-          "poet": row["Poet"],
-          "line": row["Line"],
-          "metaphor_present": row["Metaphor present or not"],
-          "metaphor_count": row["Count of the metaphor"],
-          "metaphorical_terms": str(row["Metaphorical terms"]),
-      }
-      es.index(index="poem", id=i, document=doc)
+        doc = {
+            "poem_name": row["Poem Name"],
+            "poet": row["Poet"],
+            "line": row["Line"],
+            "metaphor_present": row["Metaphor present or not"],
+            "metaphor_count": row["Count of the metaphor"],
+            "metaphorical_terms": str(row["Metaphorical terms"]),
+        }
+        es.index(index="poem", id=i, document=doc)
     except Exception as e:
-      print(print(f"An error occurred: {e}"))
-      pass
+        print(print(f"An error occurred: {e}"))
+        pass
